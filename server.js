98bc2bd5dbbc451bd24de4c5cce3c1fbdfe4c738
Vitor Leal <vitorleal1@gmail.com>
2014-05-16 01:43:43 -0300

    add methos to work with the angularjs app

--- server.js
+++ server.js
@@ -1,48 +1,69 @@
-var express = require('express'),
-    path    = require('path'),
-	  app   = express();
+var express    = require('express'),
+    bodyParser = require('body-parser')
+	  app        = express();
 
-//Index
-app.get('/', function(req, res) {
-		res.send('Hello world');
-});
 
-//Enviando html
-app.get('/about', function(req, res) {
-    res.sendfile(__dirname + '/public/about.html');
-});
+//Adiciona header que permite requests de outros dominios
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+};
+app.use(allowCrossDomain);
+
+//Faz o parser do body
+app.use(bodyParser());
+
 
 //User
-app.get('/user', function(req, res) {
-    var user = {
-      name: 'Vitor',
-      lastname: 'Leal'
-    }
-		res.send(user);
+app.get('/user', function (req, res) {
+  var user = {
+	    name   : 'Vitor Leal',
+	    address: 'Rua Lorem Ipsum Dolor 902',
+	    job    : 'Analista de Sistemas Senior',
+	    age    : '28',
+	    thumb  : 'http://lorempixel.com/120/120/sports/'
+  };
+
+	res.send(user);
 });
 
-//User passando o parâmetro
-app.get('/user/:name', function(req, res) {
-	if (!req.params.name) {
-		res.send('Você não me disse seu nome');
 
-	} else {
-	  var user = {
-      name: req.params.name
-    }
+//Amigos
+app.get('/friends', function (req, res) {
+  var friends = [
+    { name: 'David',   invited: false, thumb: 'http://lorempixel.com/50/50/sports/' },
+    { name: 'Erico',   invited: false, thumb: 'http://lorempixel.com/50/50/sports/1' },
+    { name: 'Sheldon', invited: false, thumb: 'http://lorempixel.com/50/50/sports/2' },
+    { name: 'João',    invited: false, thumb: 'http://lorempixel.com/50/50/sports/3' },
+    { name: 'Fábio',   invited: false, thumb: 'http://lorempixel.com/50/50/sports/4' },
+    { name: 'Marcos',  invited: false, thumb: 'http://lorempixel.com/50/50/sports/5' },
+    { name: 'David',   invited: false, thumb: 'http://lorempixel.com/50/50/sports/6' },
+    { name: 'Erico',   invited: false, thumb: 'http://lorempixel.com/50/50/sports/7' },
+    { name: 'Sheldon', invited: false, thumb: 'http://lorempixel.com/50/50/sports/8' },
+    { name: 'João',    invited: false, thumb: 'http://lorempixel.com/50/50/sports/9' },
+    { name: 'Fábio',   invited: false, thumb: 'http://lorempixel.com/50/50/sports/10' }
+  ];
 
-		res.send(user);
-	}
+  res.send({ friends: friends });
 });
 
 //Login
-app.get('/login', function(req, res) {
-		res.send('Essa é a pagina de login');
-});
+app.post('/login', function (req, res) {
+  if (!req.body) {
+    res.send({ login: false });
 
-//Logout
-app.get('/logout', function(req, res) {
-		res.redirect('login');
+  } else {
+    var email = req.body.email,
+        pass  = req.body.pass;
+
+    if (email === 'user@app.com' && pass == '1234') {
+      res.send({ login: true });
+
+    } else {
+      res.send({ login: false });
+    }
+  }
 });
 
 
